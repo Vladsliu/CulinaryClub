d63196455239acb21337255703be26179681a898
Vladsliu <vlad.sliusarch@gmail.com>
2023-03-01 10:33:57 +0100

    identity framework
    identity framework

--- Models/AppUser.cs
+++ Models/AppUser.cs
@@ -1,13 +1,17 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace CookingClub.Models
 {
-	public class AppUser
+	public class AppUser : IdentityUser
 	{
-		[Key]
-		public string Id { get; set; }
+		//[Key]
+		//public string Id { get; set; }6.03 tedi
 		public int? Dishes { get; set; }
 		public int? Rating { get; set; }
+		[ForeignKey("Address")]
+		public int AddressId { get; set; } 
 		public Address? Address { get; set; }
 		public ICollection<Club> Clubs { get; set; }
 		public ICollection<MasterClass> MasterClasses { get; set; }
