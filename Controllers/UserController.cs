96ca45b7418bb9cb0dcf47d0d2aaca20d12862a7
Vladsliu <vlad.sliusarch@gmail.com>
2023-04-23 09:46:52 +0200

    Chefs info added
    Chefs info added

---
+++ Controllers/UserController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CulinaryClub.Controllers
+{
+    public class UserController : Controller
+    {
+        [HttpGet("users")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Interfaces/IUserRepository.cs
@@ -0,0 +1,14 @@
+﻿using CookingClub.Models;
+
+namespace CulinaryClub.Interfaces
+{
+    public interface IUserRepository
+    {
+        Task<IEnumerable<AppUser>> GetAllUser();
+        Task<AppUser> GetUserId(string Id);
+        bool Add(AppUser user);
+        bool Update (AppUser user);
+        bool Delete(AppUser user);
+        bool Save();
+    }
+}
